0aee0e1d292b95c44d3d604ad0edd6a596051222
Shashi Penumarthy <shashi@trln.org>
2017-06-28 10:48:55 -0400

    Removed PII

--- unfilled_requests.py
+++ unfilled_requests.py
@@ -5,7 +5,6 @@ import report
 f = '/relaisdata/reports/unfilled_requests/{0}'.format(report.get_report_filename('unfilled_requests'))
 
 q = """SELECT ID_DELIVERY.REQUEST_NUMBER, ID_DELIVERY.DATE_PROCESSED, 
-              ID_DELIVERY.PATRON_NAME, ID_DELIVERY.PATRON_SURNAME, 
               ID_REQUEST.AUTHOR, ID_REQUEST.TITLE 
        FROM ID_DELIVERY INNER JOIN ID_REQUEST 
        ON ID_DELIVERY.REQUEST_NUMBER = ID_REQUEST.REQUEST_NUMBER 
@@ -16,7 +15,7 @@ q = """SELECT ID_DELIVERY.REQUEST_NUMBER, ID_DELIVERY.DATE_PROCESSED,
 	AND ID_DELIVERY.SUPPLIER_CODE_1 Like '%list exhausted%' 
 	ORDER BY ID_DELIVERY.REQUEST_NUMBER;"""
 
-columns = ['Request Number', 'Date Processed', 'Patron Name', 'Patron Surname', 'Author', 'Title']
+columns = ['Request Number', 'Date Processed', 'Author', 'Title']
 
 if __name__ == "__main__":
 	report.write_report(q, f, cols=columns)
