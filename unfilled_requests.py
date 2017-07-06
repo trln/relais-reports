f3c1094539e735df63caeb5731adac069101d355
Shashi Penumarthy <shashi@trln.org>
2017-07-06 14:43:00 -0400

    RD-26 Update query

--- unfilled_requests.py
+++ unfilled_requests.py
@@ -4,18 +4,28 @@ import report
 
 f = '/relaisdata/reports/unfilled_requests/{0}'.format(report.get_report_filename('unfilled_requests'))
 
-q = """SELECT ID_DELIVERY.REQUEST_NUMBER, ID_DELIVERY.DATE_PROCESSED, 
-              ID_REQUEST.AUTHOR, ID_REQUEST.TITLE 
-       FROM ID_DELIVERY INNER JOIN ID_REQUEST 
-       ON ID_DELIVERY.REQUEST_NUMBER = ID_REQUEST.REQUEST_NUMBER 
-       WHERE 
-	1=1 
-	/*AND ID_DELIVERY.REQUEST_NUMBER Like 'duk%'*/ 
-	AND ID_DELIVERY.DATE_PROCESSED > DATEADD(day, -1, GETDATE()) 
-	AND ID_DELIVERY.SUPPLIER_CODE_1 Like '%list exhausted%' 
-	ORDER BY ID_DELIVERY.REQUEST_NUMBER;"""
+q = """SELECT 
+  D.REQUEST_NUMBER,
+  convert(varchar, D.DATE_PROCESSED, 101) as DATE_PROCESSED, 
+  convert(varchar, D.DATE_PROCESSED, 108) as TIME_PROCESSED,
+  convert(varchar, R.DATE_SUBMITTED, 101) as DATE_RECEIVED, 
+  convert(varchar, R.DATE_SUBMITTED, 108) as TIME_RECEIVED,
+  R.AUTHOR, 
+  R.TITLE,
+  L.INSTITUTION_NAME as REQUESTING_INSTITUTION
+ FROM ID_DELIVERY D INNER JOIN ID_REQUEST R
+ ON D.REQUEST_NUMBER = R.REQUEST_NUMBER
+ JOIN ID_LIBRARY L
+ ON R.LIBRARY_ID = L.LIBRARY_ID 
+ WHERE 
+ 1=1 
+ AND D.DATE_PROCESSED > DATEADD(day, -1, GETDATE()) 
+ AND D.SUPPLIER_CODE_1 Like '%list exhausted%' 
+ ORDER BY D.REQUEST_NUMBER ;"""
 
-columns = ['Request Number', 'Date Processed', 'Author', 'Title']
+columns = ['Request Number', 'Date Processed', 'TimeProcessed', 
+           'Date Received', 'Time Received', 'Author', 'Title',
+           'Requesting Institution']
 
 if __name__ == "__main__":
 	report.write_report(q, f, cols=columns)
