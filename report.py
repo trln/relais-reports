0b08f9159c6f55b4ea3b52611de8fbde5ec5f36c
Shashi Penumarthy <shashi@trln.org>
2017-07-19 16:14:30 +0000

    Update filename generation logic and add extractors

--- report.py
+++ report.py
@@ -14,10 +14,10 @@ def conn(host=def_host, user=def_user, password=def_password, db=def_db):
 # All daily-run reports have a filename with this pattern
 def get_report_filename(file_name_suffix, ext='.csv', dt=None):
 	import datetime
-	d = datetime.date.today()
+	d = datetime.datetime.today()
 	if dt is not None:
 		d = dt
-	return d.strftime('%Y_%m_%d_{0}{1}'.format(file_name_suffix, ext))
+	return d.strftime('%Y_%m_%d_%H_%M_%S_{0}{1}'.format(file_name_suffix, ext))
 
 # Query the DB and write out SQL into a file
 def write_report(query, file_name=None, connection=None, cols=None):
@@ -31,3 +31,11 @@ def write_report(query, file_name=None, connection=None, cols=None):
 		wr.writerow(cols)
 	for row in cursor:
 		wr.writerow([unicode(s).encode("utf-8") for s in row])
+
+def extract_date_from_filename(filename):
+	import datetime
+        a = [int(i) for i in filename.split('_')[0:3]]
+        return datetime.datetime(a[0], a[1], a[2])
+
+def extract_report_name_from_filename(filename):
+        return '_'.join(filename.split('_')[-2:]).split('.')[0]
